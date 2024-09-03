e5f241b00eedeb0965a8b99e5d133cbc8969e075
sean1money <sean@1money.com>
2024-09-03 17:53:16 +0800

    feat: jsonrpsee

--- examples/jsonrpsee/main.rs
+++ examples/jsonrpsee/main.rs
@@ -1,13 +1,17 @@
 use std::net::SocketAddr;
+use std::time::Duration;
 
 use anyhow::Result;
 use dashmap::DashMap;
 use eth::{Block, EthApiServer, EthApiServerImpl};
+use hyper::body::Bytes;
 use hyper::Method;
 use jsonrpsee::http_client::HttpClient;
 use jsonrpsee::server::Server;
 use jsonrpsee::{core::client::ClientT, rpc_params};
 use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tower_http::LatencyUnit;
 use tracing::info;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -23,23 +27,34 @@ async fn main() -> Result<()> {
         .try_init()?;
 
     let server_addr = run_server().await?;
-    info!("[rpc server] Server started at {}", server_addr);
+    info!("[rpc server] Server started at {}\n\n", server_addr);
 
     // Test the server with a client.
     let url = format!("http://{}", server_addr);
-    let client = HttpClient::builder().build(url)?;
+    let middleware = tower::ServiceBuilder::new().layer(
+        TraceLayer::new_for_http()
+            .on_request(|request: &hyper::Request<_>, _span: &tracing::Span| tracing::info!(?request, "on_request"))
+            .on_body_chunk(|chunk: &Bytes, latency: Duration, _: &tracing::Span| {
+                tracing::info!(size_bytes = chunk.len(), latency = ?latency, "on_body_chunk")
+            })
+            .make_span_with(DefaultMakeSpan::new().include_headers(true))
+            .on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
+    );
+    let client = HttpClient::builder()
+        .set_http_middleware(middleware)
+        .build(url)?;
 
     let params = rpc_params!("latest", false);
     let resp: Result<Option<Block>, _> = client.request("eth_getBlockByNumber", params).await;
-    info!("===> [rpc client] Received response: {:?}", resp);
+    info!("===> [rpc client] Received response: {:?}\n\n", resp);
 
     let params = rpc_params!("safe", false);
     let resp: Result<Option<Block>, _> = client.request("eth_getBlockByNumber", params).await;
-    info!("===> [rpc client] Received response: {:?}", resp);
+    info!("===> [rpc client] Received response: {:?}\n\n", resp);
 
     let params = rpc_params!("10000", false);
     let resp: Result<Option<Block>, _> = client.request("eth_getBlockByNumber", params).await;
-    info!("===> [rpc client] Received response: {:?}", resp);
+    info!("===> [rpc client] Received response: {:?}\n\n", resp);
 
     Ok(())
 }
@@ -71,6 +86,7 @@ async fn run_server() -> Result<SocketAddr> {
     Ok(addr)
 }
 
+/// Build a dummy state.
 fn build_state() -> DashMap<String, Block> {
     let state = DashMap::new();
     state.insert(
